ec92dac01ac399f5e1a3e29dd3c2a92a46cf69a0
ShadrackBodje13 <56391856+ShadrackBodje13@users.noreply.github.com>
2020-11-27 12:47:19 +0100

    commit
    Co-Authored-By: PEIXOTO Flavio <flavio.peixoto@ynov.com>
    Co-Authored-By: tomynov <56391628+tomynov@users.noreply.github.com>

---
+++ js/Personnage.js
@@ -0,0 +1,44 @@
+
+
+class Personnage{
+
+    constructor(){
+        this.nom = "";
+        this.pv = 100;
+        this.mp = 100;
+    }
+}
+
+class Guerrier extends Personnage{
+
+    constructor(){
+        this.nom = "Guerrier";
+        this.addPv = 1,15;
+
+    }
+
+    levelUp(addPv){
+        Personnage.pv *= addPv;
+        console.log(Personnage.pv);
+    }
+}
+
+class Archer extends Personnage{
+
+    constructor(){
+        this.nom = "Archer"
+        this.pv = 100;
+        this.mp = 70;
+
+    }
+}
+
+class Ninja extends Personnage{
+
+    constructor(){
+        this.nom = "Ninja";
+        this.pv = 110;
+        this.mp = 50;
+
+    }
+}
