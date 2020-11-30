e8afa5ffdd32163d7c463c701c588640242fc2b2
Flavio Peixoto <flavio.peixoto@ynov.com>
2020-11-30 14:10:31 +0100

    Creation class monstre

---
+++ js/Monstre.js
@@ -0,0 +1,38 @@
+
+
+class Monstre{
+    constructor(nom, level, pv, mp, vitesse){
+        this.nom;
+        this.level;
+        this.pv;
+        this.mp;
+        this.vitesse;
+    }
+
+    LevelMonstre(level){
+        this.pv += ((level - 1) * 3);
+        this.mp += ((level - 1) * 2);
+        this.vitesse += ((level - 1) * 2);
+    }
+}
+
+class Gobelin extends Monstre{
+    constructor(){
+        super(nom, level, pv, mp, vitesse);
+        this.nom = "Gobelin";
+        this.pv = 50;
+        this.mp = 20;
+        this.vitesse = 10;
+    }
+}
+
+class Esprit extends Monstre{
+
+    constructor(){
+        super(nom, level, pv, mp, vitesse);
+        this.nom = "Esprit";
+        this.pv = 70;
+        this.mp = 30;
+        this.vitesse = 20;
+    }
+}
