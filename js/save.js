d204f5d6fda75859a0fa172d29b8af6b602be181
Flavio Peixoto <flavio.peixoto@ynov.com>
2020-12-11 18:19:45 +0100

    ajout local storage, conservation des donnes de joueurs quand partie non finie

--- js/save.js
+++ js/save.js
@@ -1,19 +1,5 @@
-//localStorage.removeItem("x")
-//localStorage.getItem("x",1) attribut a nb with the name "x"
-
-if (localStorage!=undefined){
-    var shop = localStorage.username
-    username = localStorage["username"]
-    localStorage.username = "test"
-
-    //localStorage["username"] --> return "test"
-}
-
-//localStorage.clear()
-
-
 //affiche l'historique des attaques en ajoutant et supprimant une div
- $( ".button" ).click(function() { 
+$( ".button" ).click(function() { 
     $( ".divHistory" ).append('<div ><p>',classe_perso.atk,'</p></div>');
     if ($(".divHistory").children().length >5){
         $(".divHistory").children()[0].remove();
@@ -21,28 +7,38 @@ if (localStorage!=undefined){
   });
 
 
+function storage(perso){
+    localStorage.atk = perso["atk"];
+    localStorage.esquive = perso["esquive"];
+    localStorage.level = perso["level"];
+    localStorage.mag = perso["mag"];
+    localStorage.maxmp = perso["maxmp"];
+    localStorage.maxpv = perso["maxpv"];
+    localStorage.mp = perso["mp"];
+    localStorage.nom = perso["nom"];
+    localStorage.or = perso["or"];
+    localStorage.prot = perso["prot"];
+    localStorage.protmag = perso["protmag"];
+    localStorage.pv = perso["pv"];
+    localStorage.vitesse = perso["vitesse"];
+}
 
-// requete post
 
-/*$.post(
-    'index.php', // Un script PHP que l'on va créer juste après
-    {
-        gold: "test"
-    },
-
-    function(data){
- 
-        if(data == 'Success'){
-             console.log("Success");
-        }
-        else{
-             console.log("Erreur !!!");
-             console.log(data);
-        }
- 
-    },
-
-    'text' // Nous souhaitons recevoir "Success" ou "Failed", donc on indique text !
- );
- */
-
+function verif_class(nom){
+    if (nom=="Ninja"){
+        var classe = new Ninja();
+        return classe;
+    }
+    else if (nom=="Magicien"){
+        var classe = new Magicien();
+        return classe;
+    }
+    else if (nom=="Guerrier"){
+        var classe = new Guerrier();
+        return classe;
+    }
+    else if (nom=="Archer"){
+        var classe = new Archer();
+        return classe;
+    }
+}
